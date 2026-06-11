1faced16bf0d9dca927f79279046c395c9219395
sachin kunwar <sachinkunwar284@gmail.com>
2026-06-11 08:25:14 +0545

    store auth state using zustand, guard routes and add product

---
+++ src/stores/authStore.js
@@ -0,0 +1,38 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+const useAuthStore = create(
+  persist(
+    (set) => ({
+      user: null,
+      isAuthenticated: false,
+
+      loginUser: ({ user }) => {
+        set({
+          user,
+          isAuthenticated: true,
+        });
+
+        localStorage.setItem("authToken", user.token);
+      },
+
+      registerUser: ({ user }) => {
+        set({
+          user,
+          isAuthenticated: true,
+        });
+
+        localStorage.setItem("authToken", user.token);
+      },
+
+      logout: () =>
+        set({
+          user: null,
+          isAuthenticated: false,
+        }),
+    }),
+    { name: "zustand:auth-storage" },
+  ),
+);
+
+export default useAuthStore;
