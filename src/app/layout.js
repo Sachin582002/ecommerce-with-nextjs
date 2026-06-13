935e4c8c45ed86aab354b0f5094b14bf4747ac52
sachin kunwar <sachinkunwar284@gmail.com>
2026-06-13 08:16:15 +0545

    products filter and search

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,12 @@
 import "./globals.css";
 import Header from "@/components/Header";
+import MainLayout from "@/layouts/MainLayout";
 import { ToastContainer } from "react-toastify";
 
 export const metadata = {
   title: {
-    default: "E-fashion",
-    template: "%s | E-fashion",
+    default: "HamroMart",
+    template: "%s | HamroMart",
   },
   description: "Online ecommerce platform for clothes and shoes.",
   keywords: "Online shopping in Nepal, Best clothing items",
@@ -13,9 +14,12 @@ export const metadata = {
 
 const RootLayout = ({ children }) => (
   <html lang="en">
-    <body className="light">
-      <Header />
-      {children}
+    <body>
+      <MainLayout>
+        <Header />
+        {children}
+      </MainLayout>
+
       <ToastContainer position="top-center" autoClose={2000} />
     </body>
   </html>
